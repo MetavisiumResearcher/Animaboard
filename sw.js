2cd8587b54020721d4c16c6d18a6a514b54a5ab3
MetavisiumResearcher <metavisium@proton.me>
2025-09-19 06:05:58 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const cacheName = 'tgho-cache-v1';
+const cacheName = 'animaboard-cache-v1';
 const filesToCache = [
   './',
-  './Animaboard',
+  './index.html',
   './manifest.json',
   './favicon.png',
   './apple-touch-icon.png',
@@ -9,10 +9,18 @@ const filesToCache = [
   './icon-512x512.png'
 ];
 
-self.addEventListener('install', e=>{
-  e.waitUntil(caches.open(cacheName).then(cache=>cache.addAll(filesToCache)));
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName)
+      .then(cache => cache.addAll(filesToCache))
+      .catch(err => console.log("Errore durante la cache:", err))
+  );
 });
 
-self.addEventListener('fetch', e=>{
-  e.respondWith(caches.match(e.request).then(r=>r || fetch(e.request)));
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request)
+      .then(r => r || fetch(e.request))
+      .catch(err => console.log("Errore durante fetch:", err))
+  );
 });
